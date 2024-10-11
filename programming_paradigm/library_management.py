7efd74ba6d885dd535110f871a9757ca7d5933b7
Priscakoech <priscakoech448@gmail.com>
2024-10-11 23:33:31 +0300

    Add main.py for testing library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,62 @@
+# Book class
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute to track availability
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+
+# Library class
+class Library:
+    def __init__(self):
+        self._books = []  # private list to store books
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    print(f"You have successfully checked out '{title}'")
+                else:
+                    print(f"'{title}' is already checked out")
+                return
+        print(f"'{title}' not found in the library")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    print(f"You have successfully returned '{title}'")
+                else:
+                    print(f"'{title}' is not currently checked out")
+                return
+        print(f"'{title}' not found in the library")
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
+        else:
+            print("No books are currently available")
+
+ 
+    
+    
