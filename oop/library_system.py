19eb01ca8230343d6aa1c04bf8f541ca9e9e34ba
Priscakoech <priscakoech448@gmail.com>
2024-10-13 22:04:31 +0300

    Add library system and main script

--- oop/library_system.py
+++ oop/library_system.py
@@ -27,10 +27,13 @@ class Library:
         self.books.append(book)  
         
     def list_books(self):
-        print("Library Collection:")
-        for book in self.books:
-            print(book)  
-
+       for book in self.books:
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
 if __name__ == "__main__":
     library = Library()
 
