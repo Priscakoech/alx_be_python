0a159f327da0b73d4d8b75a764cee9e5f6a2f4f4
Priscakoech <priscakoech448@gmail.com>
2024-10-18 22:53:53 +0300

    Add class static demo

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,11 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+    @staticmethod
+    def add(a, b):
+        """Returns the sum of two numbers."""
+        return a + b
+    @classmethod
+    def multiply(cls, a, b):
+        """Returns the product of two numbers and prints the class attributes."""
+        print(f"calculation type: {cls.calculation_type}")
+        return a * b
